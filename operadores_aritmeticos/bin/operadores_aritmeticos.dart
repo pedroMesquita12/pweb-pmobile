5729c11a0514fc1ae47626cacfe04c4d212c20c0
Pedro Mesquita <harrylk01z@gmail.com>
2026-03-23 19:22:49 +0000

    criação da pasta da primeira atividade de dart

---
+++ operadores_aritmeticos/bin/operadores_aritmeticos.dart
@@ -0,0 +1,5 @@
+import 'package:operadores_aritmeticos/operadores_aritmeticos.dart' as operadores_aritmeticos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${operadores_aritmeticos.calculate()}!');
+}
